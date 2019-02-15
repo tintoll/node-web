b025e31e3cdd7ae05331a2bbfbfdffb444e4ceb2
belselios@gmail.com <belselios@gmail.com>
2019-02-16 06:58:56 +0900

    2강

--- README.md
+++ README.md
@@ -1,1 +1,9 @@
 # node-web
+
+## 1강
+
+- url : http://blog.jeonghwan.net/series/2018/12/01/node-web-1_orientation.html
+
+## 2강
+
+- url : http://blog.jeonghwan.net/series/2018/12/02/node-web-2_http.html
--- server.js
+++ server.js
@@ -1,3 +1,19 @@
-const server = () => 'Hello World';
+const http = require("http"); // 노드 모듈을 가져온다.
 
-module.exports = server;
+const hostname = "127.0.0.1"; // 사용할 서버 호스트네임
+const port = 5000; // 사용할 서버포트
+
+// 서버를 만든다.
+const server = http.createServer((req, res) => {
+  // 요청이 오면 실행되는 콜백함수
+  res.statusCode = 200; // 응답 상태값 설정
+  res.setHeader("Content-Type", "text/plain"); //응답 헤더 중 Content-Type 설정
+  res.end("Hello, World \n"); // 응답 데이터 전송
+});
+
+// 서버를 요청 대기 상태로 만든다.
+server.listen(port, hostname, () => {
+  // 요청 대기가 완료되면 실행되는 콜백함수
+  // 터미널에 로그를 기록한다.
+  console.log(`Server running at http://${hostname}:${port}/`);
+});
