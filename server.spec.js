95447ef3700e5bd8d9c05aadbf8c9a763b0b6c44
tintoll <belselios@gmail.com>
2019-02-27 08:26:43 +0900

    삭제 파일

--- server.spec.js
+++
@@ -1,8 +0,0 @@
-const should = require("should");
-const server = require("./server");
-
-describe("server test suite", () => {
-  it('should return "Hello World"', () => {
-    server().should.be.equal("Hello World");
-  });
-});
