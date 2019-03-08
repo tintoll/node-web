7b8a232f1b03525b0dc721dd26f77e7839d4fdd1
belselios@gmail.com <belselios@gmail.com>
2019-03-08 21:19:20 +0900

    chap7

--- src/Application.js
+++ src/Application.js
@@ -2,48 +2,23 @@ const http = require('http');
 const debug = require('../utils/debug')('Application');
 const path = require('path');
 const fs = require('fs');
-
-
-const mimeType = {
-  '.ico': 'image/x-icon',
-  '.html': 'text/html',
-  '.js': 'text/javascript',
-  '.css': 'text/css',
-  '.png': 'image/png',
-  '.jpg': 'image/jpeg',
-  '.eot': 'appliaction/vnd.ms-fontobject',
-  '.ttf': 'aplication/font-sfnt'
-}
+const serveStatic = require('./serve-static');
 
 const _server = http.createServer((req, res) => {
-  
-  const ext = path.parse(req.url).ext;
-  const publicPath = path.join(__dirname, '../public');
-  // 정적 파일 처리 
-  if(Object.keys(mimeType).includes(ext)) {
-    fs.readFile(`${publicPath}${req.url}`, (err, data) => {
-      if(err) {
-        res.statusCode = 404;
-        res.end('Not found');
-      } else {
-        res.statusCode = 200;
-        res.setHeader('Content-Type', mimeType[ext]);
-        res.end(data);
-      }
-    });
-  } else {
-    res.statusCode = 200;
-    // res.setHeader('Content-Type', 'text/plain');
-    res.setHeader('Content-Type', 'text/html'); // html 렌더링하기 위해서 
+  serveStatic(req, res);
 
-    // 현재경로(__dirname)
-    const filePath = path.join(__dirname, '../public/index.html');
-    fs.readFile(filePath, (err, data) => {
-      if (err) throw err;
+  res.statusCode = 200;
+  // res.setHeader('Content-Type', 'text/plain');
+  res.setHeader('Content-Type', 'text/html'); // html 렌더링하기 위해서
 
-      res.end(data);
-    });
-  }
+  // 현재경로(__dirname)
+  const filePath = path.join(__dirname, '../public/index.html');
+  fs.readFile(filePath, (err, data) => {
+    if (err) throw err;
+
+    res.end(data);
+  });
+
 });
 
 
