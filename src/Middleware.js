ffa4bbc0cd290bdd729e8380c4c593aab964e167
belselios@gmail.com <belselios@gmail.com>
2019-03-09 21:27:19 +0900

    chap8

---
+++ src/Middleware.js
@@ -0,0 +1,37 @@
+const Middleware = () => {
+    const _middlewares = [];
+    let _req, _res;
+
+    const run = (req, res) => {
+        _req = req;
+        _res = res;
+
+        _run(0);
+    };
+
+    const _run = (i, err) => {
+        if(i < 0 || i >= _middlewares.length) return;
+
+        const nextMw = _middlewares[i];
+        const next = (err) => _run(i+1, err);
+
+        if(err) {
+            const isNextErrorMw = nextMw.length === 4;
+            return isNextErrorMw ?
+                    nextMw(err, _req, _res, next) :
+                    _run(i+1, err);
+        }
+        nextMw(_req, _res, next);
+    };
+
+    const add = fn => {
+        _middlewares.push(fn);
+    };
+    return {
+        _middlewares,
+        add,
+        run
+    };
+};
+
+module.exports = Middleware;
