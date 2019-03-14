9d5429f99477a0ee37a997dc7147a0186c1d900b
tintoll <belselios@gmail.com>
2019-03-15 07:48:36 +0900

    chapter end

--- app.js
+++ app.js
@@ -1,17 +1,19 @@
-const debug = require('./utils/debug')('app');
-const App = require('./src/Application');
+const debug = require('debug')('app');
+//const App = require('./src/Application');
+const express = require('express');
 const serveStatic = require('./middlewares/serve-static');
-const logger = require('./middlewares/logger');
+const logger = require('morgan');
 const error = require('./routes/error');
 const index = require('./routes/index');
 const apiPost = require('./routes/api/post');
-const bodyParser = require('./middlewares/body-parser');
+const bodyParser = require('body-parser');
 
-const app =  App();
+const app = express();
 
-app.use(logger());
-app.use(bodyParser());
-app.use(serveStatic());
+app.use(logger('dev'));
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
+app.use(express.static('public'));
 app.get('/', index.listPosts());
 app.get('/api/posts', apiPost.index());
 app.post("/api/posts", apiPost.create());
--- bin.js
+++ bin.js
@@ -2,7 +2,7 @@ const app = require("./app");
 const hostname = "127.0.0.1"; // 사용할 서버 호스트네임
 const port = 5000; // 사용할 서버포트
 
-const debug = require('./utils/debug')('bin');
+const debug = require('debug')('bin');
 
 // 서버를 요청 대기 상태로 만든다.
 app.listen(port, hostname, () => {
